9dfe48747ceec17e8947dd22f013b02e3ef27829
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-27 22:28:58 +0200

    Refine config selection in boot script to exclude hidden directories and adjust install script sourcing

--- boot.sh
+++ boot.sh
@@ -24,7 +24,7 @@ echo -e "\e[32m\nDotfiles cloned successfully!\e[0m"
 
 # Obtain list of folders in ~/.cfg, those are available configs to install
 DOTFILES_DIR=~/.cfg
-CONFIGS=$(find "$DOTFILES_DIR" -mindepth 1 -maxdepth 1 -type d)
+CONFIGS=$(find "$DOTFILES_DIR" -mindepth 1 -maxdepth 1 -type d -not -name ".*" -exec basename {} \;)
 
 # List the configs and ask user to select which ones to install
 echo -e "\e[32m\nAvailable Configs:\e[0m"
@@ -37,7 +37,7 @@ done
 
 if [ -n "$CONFIG" ]; then
   echo -e "\e[32m\nInstalling $CONFIG...\e[0m"
-  source "$DOTFILES_DIR/$CONFIG/install.sh"
+  source "$DOTFILES_DIR/$CONFIG/install"
   echo -e "\e[32m\nInstallation of $CONFIG completed!\e[0m"
 else
   echo -e "\e[31m\nNo valid config selected. Aborting installation.\e[0m"

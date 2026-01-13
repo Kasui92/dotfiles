f906cbcfff1dd8df36b1b8f966aaa42fe2ee08cc
Luca Pattocchio <pattocchioluca@gmail.com>
2026-01-13 15:54:28 +0100

    Refactor Fastfetch installation script to prompt user before installation

--- deb/installer/fastfetch.sh
+++ deb/installer/fastfetch.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Install Fastfetch
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
-sudo apt update -y
-sudo apt install -y fastfetch
+default_response="N"
+if ! command -v fastfetch &>/dev/null; then
+    read -p "Do you want to install Fastfetch? [y/N]: " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+      sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+      sudo apt update -y
+      sudo apt install -y fastfetch
+    fi
+fi
+
+

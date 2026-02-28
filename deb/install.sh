2ead727ccc52562b7ca5216c2a2e2c54fe0b8a31
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-28 08:34:13 +0100

    Remove redundant checks

--- deb/install.sh
+++ deb/install.sh
@@ -3,16 +3,5 @@
 # Install apps and dependencies
 INSTALLER_DIR="$HOME/.cfg/deb/install"
 
-# Install base components
 source "$INSTALLER_DIR/base.sh"
 source "$INSTALLER_DIR/apps.sh"
-
-# Add source command to .bashrc
-if [ -f ~/.bashrc ]; then
-    echo "source ~/.cfg/deb/default/bash/rc" >> ~/.bashrc
-    echo "export PATH=\"\$HOME/.cfg/deb/bin:\$PATH\"" >> ~/.bashrc
-fi
-
-# Copy configuration
-mkdir -p ~/.config/
-cp -r ~/.cfg/deb/config/* ~/.config/

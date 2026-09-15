773b002361815c8d9e9aa53505b2c4be0665fe44
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-09-15 12:14:43 +0200

    ref:  init fresh restart

--- deb/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install apps and dependencies
-INSTALLER_DIR="$HOME/.cfg/deb/install"
-
-source "$INSTALLER_DIR/base.sh"
-source "$INSTALLER_DIR/apps.sh"
--- deb/install/apps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install apps and dependencies
-APPS_DIR="$HOME/.cfg/deb/applications"
-
-# Install additional components
-for script in "$APPS_DIR"/*.sh; do
-    source "$script"
-done
